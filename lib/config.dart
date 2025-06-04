ee22902dff4f971dedb6368114a2dd98e94ae63e
duckly_ <151133220+nanathe8@users.noreply.github.com>
2025-06-04 22:21:16 +0800

    Delete lib directory

--- lib/config.dart
+++
@@ -1,4 +0,0 @@
-class CONFIG {
-  //static final String SERVER = "http://192.168.1.101/your-project-folder"; // Replace with your actual local IP address and folder path
-  static final String SERVER = "http://192.168.12.226/PSM1/ETMS";
-}
